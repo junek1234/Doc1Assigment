24a2d89e5e0f83175035f619adb8b49ec9930b13
Piotr <piotrju2005@wp.pl>
2026-04-01 15:06:13 +0200

    nothing changed to test

--- src/test/java/org/example/springbootdocassigment/SpringbootdocassigmentApplicationTests.java
+++ src/test/java/org/example/springbootdocassigment/SpringbootdocassigmentApplicationTests.java
@@ -3,11 +3,25 @@ package org.example.springbootdocassigment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SpringbootdocassigmentApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//test
+	}
+
+	@Test
+	void dummyTestShouldPassWithTrueCondition() {
+		assertTrue(true);
+	}
+
+	@Test
+	void dummyMathTestShouldPass() {
+		assertEquals(4, 2 + 2);
 	}
 
 }
