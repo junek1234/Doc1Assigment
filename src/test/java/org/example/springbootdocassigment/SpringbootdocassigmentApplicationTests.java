5206ed5ba29e18ed0a58834de0069b49f2497a7b
Piotr <piotrju2005@wp.pl>
2026-04-01 13:10:43 +0200

    Initial commit without big tar file

---
+++ src/test/java/org/example/springbootdocassigment/SpringbootdocassigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootdocassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdocassigmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
